173f6c783598cdd85aff9322a45be461e0c87fba
Geri902 <andigergo2003@gmail.com>
2026-02-23 20:55:23 +0100

    Set up basic draw logic for Segment

--- Scripts/Reworked Scripts/Segment.cs
+++ Scripts/Reworked Scripts/Segment.cs
@@ -1,15 +1,138 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
+public enum BodyType
+{
+	Straight = 0,
+	Bend = 1,
+	Tail = 2,
+	HeadNormal = 3,
+	HeadOpen = 4,
+	AttackSmall = 5,
+	AttackLarge = 6,
+}
 
 public partial class Segment : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
+	private const int size = 128;
+	private AnimatedSprite2D frames;
+	public bool isHead = false;
 	public override void _Ready()
 	{
+		frames = GetNode<AnimatedSprite2D>("Frames");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public override void _Process(double delta)
 	{
+		
+	}
+	private int CalcDeg(Vector2I currentI, Vector2I previousI, Vector2I nextI, BodyType what)
+	{
+		Vector2I diffNext;
+		Vector2I diffPrev;
+
+		int deg = 0;
+
+		switch (what)
+		{
+			case BodyType.HeadNormal:
+			case BodyType.HeadOpen:
+			case BodyType.AttackSmall:
+			case BodyType.AttackLarge:
+				diffNext = nextI - currentI;
+
+				if (diffNext == Vector2I.Down)
+				{
+					deg = 90;
+				}
+				else if (diffNext == Vector2I.Left)
+				{
+					deg = 180;
+				}
+				else if (diffNext == Vector2I.Up)
+				{
+					deg = 270;
+				}
+				break;
+			case BodyType.Tail:
+				diffPrev = previousI - currentI;
+
+				if (diffPrev == Vector2I.Up)
+				{
+					deg = 90;
+				}
+				else if (diffPrev == Vector2I.Right)
+				{
+					deg = 180;
+				}
+				else if (diffPrev == Vector2I.Down)
+				{
+					deg = 270;
+				}
+				break;	
+			default:
+				diffNext = nextI - currentI;
+				diffPrev = previousI - currentI;
+
+				if (previousI.X != nextI.X && nextI.Y != previousI.Y)
+				{
+					if ((diffPrev == Vector2I.Left && diffNext == Vector2I.Up) || (diffNext == Vector2I.Left && diffPrev == Vector2I.Up))
+					{
+						deg = 90;
+					}
+					else if ((diffPrev == Vector2I.Right && diffNext == Vector2I.Up) || (diffNext == Vector2I.Right && diffPrev == Vector2I.Up))
+					{
+						deg = 180;
+					}
+					else if ((diffPrev == Vector2I.Right && diffNext == Vector2I.Down) || (diffNext == Vector2I.Right && diffPrev == Vector2I.Down))
+					{
+						deg = 270;
+					}
+				}
+				else
+				{
+					if (diffNext == Vector2I.Left){
+						deg = 180;
+					}
+
+					else if (diffNext == Vector2I.Up){
+						deg = 270;
+					}
+
+					else if (diffNext == Vector2I.Down){
+						deg = 90;
+					}
+				}
+				break;
+		}
+
+		return deg;
+	}
+
+	private void DrawSegment(Vector2 previous, Vector2 next, BodyType what)
+	{
+		Vector2I currentI = new Vector2I((int)GlobalPosition.X / size, (int)GlobalPosition.Y / size);
+		Vector2I previousI = new Vector2I((int)previous.X / size, (int)previous.Y / size);
+		Vector2I nextI = new Vector2I((int)next.X / size, (int)next.Y / size);
+
+		int deg = 0;
+		switch (CalcDeg(currentI, previousI, nextI, what))
+		{
+			case 90:
+				deg = (int)TileSetAtlasSource.TransformTranspose | (int)TileSetAtlasSource.TransformFlipH;
+				break;
+			case 180:
+				deg = (int)TileSetAtlasSource.TransformFlipH | (int)TileSetAtlasSource.TransformFlipV;
+				break;
+			case 270:
+				deg = (int)TileSetAtlasSource.TransformTranspose | (int)TileSetAtlasSource.TransformFlipV;
+				break;
+			default:
+				break;
+		}
+		GlobalRotation = deg;
+		frames.Frame = (int)what;
 	}
 }
