93c23948cdf2684ffca8ce30fdcf6e2f55812277
Geri902 <andigergo2003@gmail.com>
2026-03-20 16:18:53 +0100

    Added intro video and logic to it

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -8,21 +8,45 @@ public partial class MainMenu : Node2D
 	[Export]
 	private PackedScene worldDestructionScene;
 	[Export]
+	private Label titleLabel;
+	[Export]
 	private Button normalButton;
 	[Export]
 	private Button worldDestructionButton;
+	[Export]
+	private VideoStreamPlayer introVideo;
 	private Control panel;
 	private Game game = null;
 	private WorldDestruction destructionGame = null;
+	private double videoLength = 0.0;
+
 	public override void _Ready()
 	{
 		normalButton.Pressed += StartNormal;
 		worldDestructionButton.Pressed += StartWorldDestruction;
 		panel = GetNode<Control>("Panel");
+
+		introVideo.Finished += EnableUI;
+
+		videoLength = Math.Floor(introVideo.GetStreamLength());
 	}
 
 	public override void _Process(double delta)
 	{
+		if(Input.IsAnythingPressed()){
+			if (Input.IsActionJustPressed("Fire"))
+			{
+				GD.Print(videoLength);
+				introVideo.StreamPosition = videoLength;
+			}
+		}
+	}
+
+	private void EnableUI()
+	{
+		titleLabel.Visible = true;
+		normalButton.Visible = true;
+		worldDestructionButton.Visible = true;
 	}
 
 	private void StartNormal()
